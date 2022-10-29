2ca5da6423e95617c7af681c09c0ea9e9cc2e71c
Joseph <jsfrnc@gmail.com>
2022-10-29 14:37:50 +0000

    add solutions for lab

---
+++ terraform-challenge/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  backend "gcs" {
+    bucket  = "tf-bucket-429439"
+    prefix  = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.55.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+
+module "instances" {
+  source     = "./modules/instances"
+}
+
+module "storage" {
+  source     = "./modules/storage"
+}
+
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "3.4.0"
+
+    project_id   = "qwiklabs-gcp-04-73b50916e9d6"
+    network_name = "tf-vpc-771525"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-east1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-east1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        }
+    ]
+}
+
+resource "google_compute_firewall" "tf-firewall" {
+  name    = "tf-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_tags = ["web"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
